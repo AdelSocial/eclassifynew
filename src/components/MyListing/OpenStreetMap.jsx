2cd3a201a2f376ac5037c2cf10a43b4c7e2619b7
Decide Precise Technologies <decideprecise@gmail.com>
2025-12-04 18:46:03 +0530

    commit

--- src/components/MyListing/OpenStreetMap.jsx
+++ src/components/MyListing/OpenStreetMap.jsx
@@ -2,6 +2,7 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "@/components/leaflet-fix";
 
 export default function OpenStreetMap({ latitude, longitude }) {
     if (!latitude || !longitude) return null;
@@ -12,7 +13,11 @@ export default function OpenStreetMap({ latitude, longitude }) {
             zoom={13}
             style={{ height: "200px", width: "100%", borderRadius: "8px" }}
         >
-            <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap" />
+              <TileLayer 
+                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                attribution="&copy; OpenStreetMap" 
+            />
+            {/* <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap" /> */}
             <Marker position={[latitude, longitude]}>
                 <Popup>Location</Popup>
             </Marker>
